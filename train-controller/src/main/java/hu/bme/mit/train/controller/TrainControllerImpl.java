ed0aea18349cbc065b9c65520a87af62ea7dd14e
Pelles Kinga <pelles_kinga@msn.com>
2019-03-25 07:16:07 -0700

    ok

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -1,5 +1,8 @@
 package hu.bme.mit.train.controller;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import hu.bme.mit.train.interfaces.TrainController;
 
 public class TrainControllerImpl implements TrainController {
@@ -7,6 +10,18 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+
+	Timer timer = new Timer();
+
+
+	public TrainControllerImpl(){
+		timer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+			followSpeed();
+			}
+		}, 2*60*1000, 2*60*1000);
+	}
 
 	@Override
 	public void followSpeed() {
